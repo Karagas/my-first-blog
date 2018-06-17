b68bef3dd2b671c88f1219e35c37ae003af10c77
Karagas <thomas_dias@hotmail.com>
2018-06-17 09:13:08 +0000

    Create/edit added

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    theme = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
